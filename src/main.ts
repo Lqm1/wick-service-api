4c7b38417cdd58c7e4a3165aa0e573ddabf5c31c
Lqm1 <154405627+Lqm1@users.noreply.github.com>
2025-12-26 20:00:26 +0900

    ✨ main.tsから不要なコードを削除し、WickServiceとWickServiceErrorをエクスポート コードの整理と可読性の向上を図るために、不要な関数を削除しました。

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+export { WickService } from "./wick-service.ts";
+export { WickServiceError } from "./errors.ts";
+export * from "./types.ts";
