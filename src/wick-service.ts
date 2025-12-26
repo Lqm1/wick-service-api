7bdd7189b34be249ca11c895fafe75d7fa06392b
Lqm1 <154405627+Lqm1@users.noreply.github.com>
2025-12-26 21:05:36 +0900

    ✨ WickServiceクラスにSNS機能を追加し、WickServiceSNSを統合 SNS関連のAPIを利用できるようにWickServiceクラスを拡張する。

--- src/wick-service.ts
+++ src/wick-service.ts
@@ -1,14 +1,17 @@
 import { WickServiceAuth } from "./wick-service-auth.ts";
 import { WickServiceUser } from "./wick-service-user.ts";
+import { WickServiceSNS } from "./wick-service-sns.ts";
 import { WickServiceBase } from "./wick-service-base.ts";
 
 export class WickService extends WickServiceBase {
   public readonly auth: WickServiceAuth;
   public readonly user: WickServiceUser;
+  public readonly sns: WickServiceSNS;
 
   constructor(proxyUrl?: string) {
     super(proxyUrl);
     this.auth = new WickServiceAuth(this.instance);
     this.user = new WickServiceUser(this.instance);
+    this.sns = new WickServiceSNS(this.instance);
   }
 }
