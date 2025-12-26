6acb0b0f5f488cd396da21d7f6d9d9b7eb4707a3
Lqm1 <154405627+Lqm1@users.noreply.github.com>
2025-12-26 17:28:01 +0900

    ✨ 定数ファイルにWickサービスのAPI URLを追加 プロダクション、プレプロダクション、ステージング環境のAPI URLを定義し、コードの可読性と管理を向上させる。

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const PROD_API_WICK_SERVICE_URL = "https://prod.api.wick-service.com";
+export const PRE_PROD_API_WICK_SERVICE_URL =
+  "https://pre-prod.api.wick-service.com";
+export const STG_API_WICK_SERVICE_URL = "https://stg.api.wick-service.com";
